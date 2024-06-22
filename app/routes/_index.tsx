c66db93760ec34f39a346aeebe84806706fee494
Lucas Munhoz <ln.munhoz@gmail.com>
2024-06-22 14:51:57 +0700

    Encoding URI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,126 +8,144 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const rawPath = (path: string) => {
+  return import.meta.env.DEV
+    ? path
+    : `https://github.com/lnmunhoz/listen-thai-words/raw/main/public${encodeURI(
+        path
+      )}`;
+};
+
 const vocabulary = [
   {
     id: 1,
     thai: "สวัสดีค่ะ ครับ",
     english: "Hello",
-    src: "/assets/audio/01_สวัสดีค่ะ ครับ (Hello).mp3",
+    src: rawPath("/assets/audio/01_สวัสดีค่ะ ครับ (Hello).mp3"),
   },
   {
     id: 2,
     thai: "คุณชื่ออะไรคะ ครับ",
     english: "What is your name",
-    src: "/assets/audio/02_คุณชื่ออะไรคะ ครับ (What is your name).mp3",
+    src: rawPath("/assets/audio/02_คุณชื่ออะไรคะ ครับ (What is your name).mp3"),
   },
   {
     id: 3,
     thai: "ผมชื่อ ฉันชื่อ เราชื่อ",
     english: "My name is",
-    src: "/assets/audio/03_ผมชื่อ ฉันชื่อ เราชื่อ My name is.mp3",
+    src: rawPath("/assets/audio/03_ผมชื่อ ฉันชื่อ เราชื่อ My name is.mp3"),
   },
   {
     id: 4,
     thai: "ยินดีที่ได้รู้จัก",
     english: "Nice to meet you",
-    src: "/assets/audio/04_ยินดีที่ได้รู้จัก (Nice to meet you).mp3",
+    src: rawPath("/assets/audio/04_ยินดีที่ได้รู้จัก (Nice to meet you).mp3"),
   },
   {
     id: 5,
     thai: "มาจากไหน มาจากประเทศอะไร",
     english: "Where are you from",
-    src: "/assets/audio/05_มาจากไหน มาจากประเทศอะไร (Where are you from).mp3",
+    src: rawPath(
+      "/assets/audio/05_มาจากไหน มาจากประเทศอะไร (Where are you from).mp3"
+    ),
   },
   {
     id: 6,
     thai: "ขอบคุณค่ะ ครับ",
     english: "Thank you",
-    src: "/assets/audio/06_ขอบคุณค่ะ ครับ (Thank you).mp3",
+    src: rawPath("/assets/audio/06_ขอบคุณค่ะ ครับ (Thank you).mp3"),
   },
   {
     id: 7,
     thai: "ขอบคุณมากๆ",
     english: "Thank you very much",
-    src: "/assets/audio/07_ขอบคุณมากๆ (Thank you very much).mp3",
+    src: rawPath("/assets/audio/07_ขอบคุณมากๆ (Thank you very much).mp3"),
   },
   {
     id: 8,
     thai: "ขอโทษค่ะ ครับ",
     english: "Sorry",
-    src: "/assets/audio/08_ขอโทษค่ะ ครับ (Sorry).mp3",
+    src: rawPath("/assets/audio/08_ขอโทษค่ะ ครับ (Sorry).mp3"),
   },
   {
     id: 9,
     thai: "ขอทางหน่อยค่ะ",
     english: "Excuse me",
-    src: "/assets/audio/09_ขอทางหน่อยค่ะ (Excuse me).mp3",
+    src: rawPath("/assets/audio/09_ขอทางหน่อยค่ะ (Excuse me).mp3"),
   },
   {
     id: 10,
     thai: "ยินดี ด้วยความยินดี",
     english: "with my pleasure",
-    src: "/assets/audio/10_ยินดี ด้วยความยินดี (with my pleasure).mp3",
+    src: rawPath("/assets/audio/10_ยินดี ด้วยความยินดี (with my pleasure).mp3"),
   },
   {
     id: 11,
     thai: "ไม่เป็นไร",
     english: "no worries",
-    src: "/assets/audio/11_ไม่เป็นไร (no worries).mp3",
+    src: rawPath("/assets/audio/11_ไม่เป็นไร (no worries).mp3"),
   },
   {
     id: 12,
     thai: "ไม่มีปัญหา",
     english: "no problem",
-    src: "/assets/audio/12_ไม่มีปัญหา (no problem).mp3",
+    src: rawPath("/assets/audio/12_ไม่มีปัญหา (no problem).mp3"),
   },
   {
     id: 13,
     thai: "คุณพูดภาษาไทยได้มั้ย",
     english: "Can you speak Thai",
-    src: "/assets/audio/13_คุณพูดภาษาไทยได้มั้ย (Can you speak Thai).mp3",
+    src: rawPath(
+      "/assets/audio/13_คุณพูดภาษาไทยได้มั้ย (Can you speak Thai).mp3"
+    ),
   },
   {
     id: 14,
     thai: "คุุณพูดภาษาอังกฤษได้มั้ย",
     english: "Can you speak English",
-    src: "/assets/audio/14_คุุณพูดภาษาอังกฤษได้มั้ย (Can you speak English).mp3",
+    src: rawPath(
+      "/assets/audio/14_คุุณพูดภาษาอังกฤษได้มั้ย (Can you speak English).mp3"
+    ),
   },
   {
     id: 15,
     thai: "พูดได้นิดหน่อย",
     english: "I can speak a little",
-    src: "/assets/audio/15_พูดได้นิดหน่อย (I can speak a little).mp3",
+    src: rawPath("/assets/audio/15_พูดได้นิดหน่อย (I can speak a little).mp3"),
   },
   {
     id: 16,
     thai: "พูดไม่ได้",
     english: "I can't speak",
-    src: "/assets/audio/16_พูดไม่ได้ (I can_t speak..).mp3",
+    src: rawPath("/assets/audio/16_พูดไม่ได้ (I can_t speak..).mp3"),
   },
   {
     id: 17,
     thai: "ฉัน(ผม)เป็นคนอเมริกา",
     english: "I am American",
-    src: "/assets/audio/17_ฉัน(ผม)เป็นคนอเมริกา (I am American).mp3",
+    src: rawPath("/assets/audio/17_ฉัน(ผม)เป็นคนอเมริกา (I am American).mp3"),
   },
   {
     id: 18,
     thai: "คุณเป็นคนไทยใช่มั้ย",
     english: "Are you Thai",
-    src: "/assets/audio/18_คุณเป็นคนไทยใช่มั้ย (Are you Thai).mp3",
+    src: rawPath("/assets/audio/18_คุณเป็นคนไทยใช่มั้ย (Are you Thai).mp3"),
   },
   {
     id: 19,
     thai: "สอนภาษาไทยให้ผมได้มั้ย",
     english: "Can you teach me Thai",
-    src: "/assets/audio/19_สอนภาษาไทยให้ผมได้มั้ย (Can you teach me Thai).mp3",
+    src: rawPath(
+      "/assets/audio/19_สอนภาษาไทยให้ผมได้มั้ย (Can you teach me Thai).mp3"
+    ),
   },
   {
     id: 20,
     thai: "ผมอยากเรียนภาษาไทย",
     english: "I want to learn Thai",
-    src: "/assets/audio/20_ผมอยากเรียนภาษาไทย (I want to learn Thai).mp3",
+    src: rawPath(
+      "/assets/audio/20_ผมอยากเรียนภาษาไทย (I want to learn Thai).mp3"
+    ),
   },
 ];
 
