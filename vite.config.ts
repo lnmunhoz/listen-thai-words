b6064977ebb44231666a299ba9bfbe124c0c251b
Lucas Munhoz <ln.munhoz@gmail.com>
2024-06-22 14:18:15 +0700

    Setup gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const ghPagesPath = "/listen-thai-words/";
+
 export default defineConfig({
+  base: ghPagesPath,
   plugins: [
     remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
+      basename: ghPagesPath,
+      ssr: false,
+      // future: {
+      //   v3_fetcherPersist: true,
+      //   v3_relativeSplatPath: true,
+      //   v3_throwAbortReason: true,
+      // },
     }),
     tsconfigPaths(),
   ],
